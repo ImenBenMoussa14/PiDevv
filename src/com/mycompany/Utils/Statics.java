fcb07ef6343a95b26d7762f73daa40132e496f4b
Imen Ben Moussa <64868764+ImenBenMoussa14@users.noreply.github.com>
2020-05-15 00:19:22 +0100

    update utils

--- src/com/mycompany/Utils/Statics.java
+++ src/com/mycompany/Utils/Statics.java
@@ -13,6 +13,7 @@ public class Statics {
        public static final String BASE_URL="http://localhost/projet_symfony/final/test/web/app_dev.php/api/mobile/";
        
         public static Utilisateur currentUser=new Utilisateur(14);
+ //   public static Utilisateur currentUser=new Utilisateur(SessionManager.getId());
 }
 
 
